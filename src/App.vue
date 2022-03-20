f17284f25de9d28dd0d3d401cfbc1897f146866f
aydar.mazitov <aydar.mazitov@gmail.com>
2022-03-20 18:35:36 +0500

    Изучение компонентов, методов, слушателя событий.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+//Single file component
+
+<template>
+    <div>
+        <button v-on:click="addLike">Like</button>
+        <button @click="addDislike">Dislike</button>
+        <div>Количество лайков: <strong>{{ likes }}</strong></div>
+        <div>Количество дизлайков: <strong>{{ dislikes }}</strong></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            likes: 0,
+            dislikes: 0
+        }
+    },
+    methods: {
+        addLike(){
+            this.likes += 1;
+        },
+        addDislike(){
+            this.dislikes +=1;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
