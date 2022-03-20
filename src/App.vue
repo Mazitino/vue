9491facbc30a2b61f5bfaf6b45b9ab3805f9f932
aydar.mazitov <aydar.mazitov@gmail.com>
2022-03-20 21:03:15 +0500

    Практика обемна данными между компонентами

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="app">
-        <post-form/>
-        <post-list :posts="posts"/>
+        <post-form
+             @create="createPost"
+        />
+        <post-list 
+            :posts="posts"
+           
+        />
     </div>
 </template>
 
@@ -22,21 +27,11 @@ export default {
                 {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
                 {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
             ],
-            title: '',
-            body: ''
         }
     },
     methods: {
-        createPost(){
-            const newPost = {
-                id: Date.now(),
-                title: this.title,
-                body: this.body
-            }
-            this.posts.push(newPost);
-            this.title = '';
-            this.body = '';
-
+        createPost(post){
+            this.posts.push(post);
         }
     }
 
--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -2,21 +2,20 @@
     <form @submit.prevent> 
         <h4>Создание поста</h4>
         <input
-            v-bind:value="post.title" 
-            @input="post.title = $event.target.value"
+            v-model="post.title"
             class="input" 
             type="text" 
             placeholder="Название"
         >
         <input 
-            v-bind:value="post.body" 
-            @input="post.body = $event.target.value"
+            v-model="post.body"
             class="input" 
             type="text" 
             placeholder="Описание"
         >
         <button 
             class="btn" 
+            @click="createPost"
             >
                 Создать
         </button>
@@ -33,6 +32,18 @@ export default {
             }
         }
     },
+    methods: {
+        createPost() {
+            this.post.id = Date.now();
+            this.$emit('create', this.post)
+            this.post = {
+                title: '',
+                body: ''
+            }
+        
+
+        }
+    }
 }
 </script>
 
