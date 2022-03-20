ce1a6a6c0395e380d669594c43fd81a13d24e204
aydar.mazitov <aydar.mazitov@gmail.com>
2022-03-20 20:19:54 +0500

    Создание постов. Создал app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,103 @@
-//Single file component
-
 <template>
-    <div>
-        <button v-on:click="addLike">Like</button>
-        <button @click="addDislike">Dislike</button>
-        <div>Количество лайков: <strong>{{ likes }}</strong></div>
-        <div>Количество дизлайков: <strong>{{ dislikes }}</strong></div>
+    <div class="app">
+        
+        <form @submit.prevent> 
+            <h4>Создание поста</h4>
+            <input
+                v-bind:value="title" 
+                @input="title = $event.target.value"
+                class="input" 
+                type="text" 
+                placeholder="Название"
+            >
+            <input 
+                v-bind:value="body" 
+                @input="body = $event.target.value"
+                class="input" 
+                type="text" 
+                placeholder="Описание"
+            >
+            <button 
+                class="btn" 
+                @click="createPost"
+                >
+                    Создать
+            </button>
+        </form>
+
+        <div class="post" v-for="post in posts">
+            <div><strong>Название: </strong> {{ post.title }} </div>
+            <div><strong>Описание: </strong> {{ post.body }} </div>
+        </div>
+
     </div>
 </template>
+
+
 
 <script>
 export default {
     data(){
         return {
-            likes: 0,
-            dislikes: 0
+            posts: [
+                {id: 1, title: 'JavaScript 1', body: 'Описание поста 1'},
+                {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
+                {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
+                {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
+            ],
+            title: '',
+            body: ''
         }
     },
     methods: {
-        addLike(){
-            this.likes += 1;
-        },
-        addDislike(){
-            this.dislikes +=1;
+        createPost(){
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body
+            }
+            this.posts.push(newPost);
+            this.title = '';
+            this.body = '';
+
         }
     }
+
 }
 </script>
 
-<style>
 
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.app{
+    padding: 20px;
+}
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+}
+form{
+    display: flex;
+    flex-direction: column;
+}
+.input {
+    width: 100%;
+    border: 2px solid teal;
+    padding: 18px 15px;
+    margin-top: 15px;
+    }
+.btn {
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 18px 30px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+}
 </style>
