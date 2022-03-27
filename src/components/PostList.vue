b196059ac9d95103f5e368a9c8c71e732c17cbd5
aydar.mazitov <aydar.mazitov@gmail.com>
2022-03-27 15:04:35 +0500

    Добавление анимации к постам

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
     <div v-if="posts.length > 0">
         <h3>Список постов</h3>
-        <post-item  
-            v-for="post in posts" 
-           :post="post"
-           :key="post.id"
-           @remove="$emit('remove',post)"
-        />
+        <transition-group name="post-list">
+            <post-item  
+                v-for="post in posts" 
+            :post="post"
+            :key="post.id"
+            @remove="$emit('remove',post)"
+            />
+        </transition-group>
     </div>
     <h2 v-else style="color: red">
         Список постов пуст
@@ -29,5 +31,20 @@ export default {
 </script>
 
 <style scoped>
-
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.3s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(200px);
+}
+.post-list-move {
+  transition: transform 0.5s ease;
+}
 </style>
